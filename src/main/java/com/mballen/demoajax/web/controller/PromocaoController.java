95ffea5292ea1a10fa8eb772592a5043f04489e1
Arthur <arthur.04.pb@gmail.com>
2019-08-08 16:41:46 -0300

    listagem de promoções prontas

--- src/main/java/com/mballen/demoajax/web/controller/PromocaoController.java
+++ src/main/java/com/mballen/demoajax/web/controller/PromocaoController.java
@@ -4,20 +4,22 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.mballen.demoajax.web.model.Categoria;
 import com.mballen.demoajax.web.model.Promocao;
@@ -63,5 +65,25 @@ public class PromocaoController {
 			promocaoRepository.save(promocao);
 			return ResponseEntity.ok().build();
 		}
+	}
+	
+	@GetMapping("/list")
+	public String listarOfertas(ModelMap model) {
+		//ModelAndView model = new ModelAndView();
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		//model.setViewName("/promo-list");
+		return "promo-list";
+	}
+	
+	@GetMapping("/list/ajax")
+	public String listarCards(@RequestParam(name = "page", defaultValue = "1") int page, ModelMap model) {
+		//ModelAndView model = new ModelAndView();
+		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(page, 8, sort);
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		//model.setViewName("/promo-list");
+		return "promo-card";
 	}
 }
---
+++ src/main/resources/static/js/promo-list.js
@@ -0,0 +1,54 @@
+var pageNumber = 0;
+
+$(document).ready(function(){
+	$("#loader-img").hide();
+	$("#fim-btn").hide();
+});
+
+//efeito infinite scroll
+$(window).scroll(function(){
+	var scrollTop = $(this).scrollTop();
+	var conteudo = $(document).height() - $(window).height();
+	
+	//console.log("scrollTop: ", scrollTop, "| conteudo: ", conteudo);
+	
+	if(scrollTop >= conteudo){
+		pageNumber++;
+		setTimeout(function(){
+			loadByScrollBar(pageNumber);
+		}, 200);
+	}
+});
+
+function loadByScrollBar(pageNumber){
+	$.ajax({
+		method: "GET",
+		url:"/promocao/list/ajax",
+		data:{
+			page: pageNumber
+		},
+		beforeSend: function(){
+			$("#loader-img").show();
+		},
+		success: function( response ){
+			//console.log("resposta: ", response );
+			console.log("lista: ", response.length );			
+			if(response.length > 150){
+				$(".row").fadeIn(250, function(){
+					$(this).append(response);
+				});
+			}else{
+				$("#fim-btn").show();
+				$("#loader-img").removeClass("loader");
+			}
+			
+		},
+		error: function(xhr){
+			alert("Opa, ocorreu um erro inesperado: " + xhr.status + " - " + xhr.statusText);
+		},
+		complete: function(){
+			$("#loader-img").hide();
+		}
+		
+	});
+};
